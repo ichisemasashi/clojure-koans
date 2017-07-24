a6fe780472cd3cace3ce3c648a2bcc2a0793a4df
ichisemasashi <ichise.masashi@gmail.com>
2017-07-24 16:02:43 +0900

    02 wip.

--- src/koans/02_strings.clj
+++ src/koans/02_strings.clj
@@ -3,44 +3,57 @@
             [clojure.string :as string]))
 
 (meditations
-  "A string is nothing more than text surrounded by double quotes"
-  (= __ "hello")
+  "A string is nothing more than text surrounded by double quotes
+  文字列とは、ダブルクォートで囲まれたテキストのことだ"
+  (= "hello" "hello")
 
-  "But double quotes are just magic on top of something deeper"
-  (= __ (str 'world))
+  "But double quotes are just magic on top of something deeper
+   しかし、ダブルクオートは何か深いものの上にある単なる魔法だ"
+  (= "world" (str 'world))
 
-  "You can do more than create strings, you can put them together"
-  (= "Cool right?" (str __ __))
+  "You can do more than create strings, you can put them together
+  文字列をつくるだけでなく、それらを結合することができる"
+  (= "Cool right?" (str "Cool " "right?"))
 
-  "You can even get certain characters"
-  (= \C (get "Characters" __))
+  "You can even get certain characters
+  さらに、特定の文字を取得することもできる"
+  (= \C (get "Characters" 0))
 
-  "Or even count the characters"
-  (= __ (count "Hello World"))
+  "Or even count the characters
+   また、更に文字数を数えることもできる"
+  (= 11 (count "Hello World"))
 
-  "But strings and characters are not the same"
-  (= __ (= \c "c"))
+  "But strings and characters are not the same
+  ただし、文字列と文字は同じものではない"
+  (= false (= \c "c"))
 
-  "What if you only wanted to get part of a string?"
-  (= "World" (subs "Hello World" __ __))
+  "What if you only wanted to get part of a string?
+  ある文字列の一部だけを取得するとどうなるか"
+  (= "World" (subs "Hello World" 6 11))
 
-  "How about joining together elements in a list?"
-  (= __ (string/join '(1 2 3)))
+  "How about joining together elements in a list?
+  あるリストの要素をジョインするとどうなるか"
+  (= "123" (string/join '(1 2 3)))
 
-  "What if you wanted to separate them out?"
-  (= "1, 2, 3" (string/join __ '(1 2 3)))
+  "What if you wanted to separate them out?
+  区切り文字を指定したいときは？"
+  (= "1, 2, 3" (string/join ", " '(1 2 3)))
 
-  "Maybe you want to separate out all your lines"
-  (= [__ __ __] (string/split-lines "1\n2\n3"))
+  "Maybe you want to separate out all your lines
+  たぶん、各行ごとに分割したいよね"
+  (= ["1" "2" "3"] (string/split-lines "1\n2\n3"))
 
-  "You may want to make sure your words are backwards"
-  (= __ (string/reverse "hello"))
+  "You may want to make sure your words are backwards
+  文字列を後ろから確認したいでしょう"
+  (= "olleh" (string/reverse "hello"))
 
-  "Maybe you want to find the index of the first occurence of a substring"
-  (= 0 (string/index-of "hello world" __))
+  "Maybe you want to find the index of the first occurence of a substring
+  ある文字列が最初に発生するインデックスを探したいでしょう"
+  (= 0 (string/index-of "hello world" "hello"))
 
-  "Or maybe the last index of the same"
-  (= __ (string/last-index-of "hello world, hello" "hello"))
+  "Or maybe the last index of the same
+  または、同様な最後のインデックス"
+  (= 13 (string/last-index-of "hello world, hello" "hello"))
 
   "But when something doesn't exist, nothing is found"
   (= __ (string/index-of "hello world" "bob"))
