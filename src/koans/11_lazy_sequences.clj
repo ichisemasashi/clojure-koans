56c4b89b488d5e49b99723fb04f2047257dd57a0
ichisemasashi <ichise.masashi@gmail.com>
2017-08-12 22:02:07 +0900

    wip 12

--- src/koans/11_lazy_sequences.clj
+++ src/koans/11_lazy_sequences.clj
@@ -2,27 +2,34 @@
   (:require [koan-engine.core :refer :all]))
 
 (meditations
-  "There are many ways to generate a sequence"
-  (= __ (range 1 5))
+  "There are many ways to generate a sequence
+   シーケンスを生成する方法はいろいろある"
+  (= '(1 2 3 4) (range 1 5))
 
-  "The range starts at the beginning by default"
-  (= __ (range 5))
+  "The range starts at the beginning by default
+   rangeはデフォルトで始めから始める"
+  (= '(0 1 2 3 4) (range 5))
 
-  "Only take what you need when the sequence is large"
+  "Only take what you need when the sequence is large
+   シーケンスが大きいときに必要なのはtake"
   (= [0 1 2 3 4 5 6 7 8 9]
-     (take __ (range 100)))
+     (take 10 (range 100)))
 
-  "Or limit results by dropping what you don't need"
+  "Or limit results by dropping what you don't need
+   または、不要なものを切り捨てて答えを求める"
   (= [95 96 97 98 99]
-     (drop __ (range 100)))
+     (drop 95 (range 100)))
 
-  "Iteration provides an infinite lazy sequence"
-  (= __ (take 20 (iterate inc 0)))
+  "Iteration provides an infinite lazy sequence
+   iterateは無限遅延シーケンスを提供する"
+  (= [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19] (take 20 (iterate inc 0)))
 
-  "Repetition is key"
+  "Repetition is key
+   繰り返しはキーです"
   (= [:a :a :a :a :a :a :a :a :a :a]
-     (repeat 10 __))
+     (repeat 10 :a))
 
-  "Iteration can be used for repetition"
+  "Iteration can be used for repetition
+   iterateは繰り返しとして使うことができる"
   (= (repeat 100 :hello)
-     (take 100 (iterate ___ :hello))))
+     (take 100 (iterate conj :hello))))
